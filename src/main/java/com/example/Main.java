eea926f1c85bb50389f6d502a1adfba15d3c1aeb
zxm <34710758+activepirate@users.noreply.github.com>
2023-07-04 09:23:20 +0800

    fix somethion
    fix #6

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,6 +3,7 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Hello fix-0704-1");
         System.out.println("Hello fix-4");
         System.out.println("Hello travis-ci");
     }
